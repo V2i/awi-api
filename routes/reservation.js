fce1c7e60647f59bc95c9a39121d1e346e2868d0
Valentin Guyon <v.guyon@fraudio.com>
2021-03-11 18:53:55 +0100

    FIX: deployment

--- routes/reservation.js
+++ routes/reservation.js
@@ -1,5 +1,99 @@
 const express = require('express');
 const router = express.Router();
 
+const Reservation = require('../models/Reservation');
+const Exhibitor = require('../models/Exhibitor');
+
+/* GET reservations listing */
+router.get('/list', async (req, res) => {
+
+    try {
+        const reservations = await Reservation.find();
+        return res.status(200).json(reservations);
+    } catch (err) {
+        return res.status(500).json({message: err});
+    }
+
+});
+
+/* GET reservations listing by festival id*/
+router.get('/list/festival/:id', async (req, res) => {
+
+    try {
+        const reservations = await Reservation.find({reservationFestival: req.params.id});
+        return res.status(200).json(reservations);
+    } catch (err) {
+        return res.status(500).json({message: err});
+    }
+
+});
+
+/* GET reservation by id */
+router.get('/:id', async (req, res) => {
+
+    try {
+        const reservation = await Reservation.findById(req.params.id);
+        return res.status(200).json(reservation);
+    } catch (err) {
+        return res.status(500).json({message: err});
+    }
+
+});
+
+/* POST new reservation */
+router.post('/', async (req, res) => {
+
+    const reservation = new Reservation({
+        reservationExhibitor: req.body.reservationExhibitor,
+        reservationReservedSpace: req.body.reservationReservedSpace,
+        reservationFestival: req.body.reservationFestival,
+        reservationTracking: req.body.reservationTracking,
+        reservationComment: req.body.reservationComment,
+        reservationGame: req.body.reservationGame,
+        reservationBilling: req.body.reservationBilling,
+        exhibitorVolunteerNeeded: req.body.exhibitorVolunteerNeeded,
+        exhibitorIsMoving: req.body.exhibitorIsMoving,
+    });
+
+    try {
+        const savedExhibitor = await exhibitor.save();
+        return res.status(200).json(savedExhibitor);
+    } catch (err) {
+        return res.status(500).json({message: err});
+    }
+
+});
+
+/* PATCH  exhibitor by id */
+router.patch('/:id', async (req, res) => {
+
+    try{
+        await Exhibitor.updateOne({_id: req.params.id}, {$set: {...req.body}})
+        const updatedExhibitor = await Exhibitor.findById(req.params.id);
+        return res.status(200).json(updatedExhibitor);
+    } catch (err) {
+        return res.status(500).json({message: err});
+    }
+
+});
+
+/* DELETE exhibitor by id */
+router.delete('/:id', async (req, res) => {
+
+    try{
+        let exhibitor = await Exhibitor.findById(req.params.id);
+
+        if(!exhibitor){
+            return res.status(403).json({message: "Object Not Found"}).end()
+        }
+        await Exhibitor.deleteOne(exhibitor);
+        return res.status(200).json(exhibitor);
+    } catch (err) {
+        return res.status(500).json({message: err});
+    }
+
+
+
+});
 
 module.exports = router;
