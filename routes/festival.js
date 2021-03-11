cb18c8d33ac3505ca0d568ec08fde4fd2749d338
Valentin Guyon <v.guyon@fraudio.com>
2021-03-11 11:07:46 +0100

    ADD: post, get, delete, patch

--- routes/festival.js
+++ routes/festival.js
@@ -1,5 +1,69 @@
 const express = require('express');
 const router = express.Router();
+const moment = require('moment');
 
+const Festival = require('../models/Festival');
+
+/* GET festival listing */
+router.get('/list', async (req, res) => {
+
+    try {
+        const festivals = await Festival.find();
+        return res.status(200).json(festivals);
+    } catch (err) {
+        return res.status(500).json({message: err});
+    }
+
+});
+
+/* POST new festival */
+router.post('/', async (req, res) => {
+
+    const festival = new Festival({
+        festivalName: req.body.festivalName,
+        festivalDate: moment(req.body.festivalDate),
+        festivalSpace: req.body.festivalSpace,
+    });
+
+    try {
+        const savedFestival = await festival.save();
+        return res.status(200).json(savedFestival);
+    } catch (err) {
+        return res.status(500).json({message: err});
+    }
+
+});
+
+/* PATCH  festival by id */
+router.patch('/:id', async (req, res) => {
+
+    try{
+        await Festival.updateOne({_id: req.params.id}, {$set: {...req.body}})
+        const updatedFestival = await Festival.findById(req.params.id);
+        return res.status(200).json(updatedFestival);
+    } catch (err) {
+        return res.status(500).json({message: err});
+    }
+
+});
+
+/* DELETE festival by id */
+router.delete('/:id', async (req, res) => {
+
+    try{
+        let festival = await Festival.findById(req.params.id);
+
+        if(!festival){
+            return res.status(403).json({message: "Object Not Found"}).end()
+        }
+        await Festival.remove(festival);
+        return res.status(200).json(festival);
+    } catch (err) {
+        return res.status(500).json({message: err});
+    }
+
+
+
+});
 
 module.exports = router;
