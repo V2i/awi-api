48bc57ac83222800fd26fd40afd15d68f8e35608
Valentin Guyon <v.guyon@fraudio.com>
2021-03-15 22:43:14 +0100

    ADD: login and register w/ bcrypt and jwt

--- routes/auth.js
+++ routes/auth.js
@@ -1,11 +1,26 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 
 const User = require('../models/User');
 
 router.post('/login', async (req, res) => {
 
-  await res.status(200).send('respond with a resource');
+    //checking if email exists
+    const user = await User.findOne({userMail: req.body.userMail});
+    if (!user) return res.status(400).send('Email or password is wrong.');
+
+    //if the passwords are matching
+    const validPassword = await bcrypt.compare(req.body.userPassword, user.userPassword);
+    if (!validPassword) return res.status(400).send('Email or password is wrong.');
+
+    //creation of the jwt
+    const token = jwt.sign({_id: user._id}, process.env.TOKEN_SECRET);
+
+    let userInfo = await User.findById(user._id);
+
+    res.status(200).send({userInfo, authToken: token});
 
 });
 
