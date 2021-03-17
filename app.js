5dcf9930ca9c51934776b18278cc5d2494c24153
Valentin Guyon <v.guyon@fraudio.com>
2021-03-17 22:22:55 +0100

    FIX: cleaning and new routes

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const mongoose = require('mongoose');
+const app = express();
 
+/* Routes */
 const authRouter = require('./routes/auth');
 const userRouter = require('./routes/user');
 const festivalRouter = require('./routes/festival');
@@ -11,15 +13,17 @@ const reservationRouter = require('./routes/reservation');
 const editorRouter = require('./routes/editor');
 const exhibitorRouter = require('./routes/exhibitor');
 const contactRouter = require('./routes/contact');
-const spaceFestival = require('./routes/space');
+const spaceRouter = require('./routes/space');
+const reservedGameRouter = require('./routes/reservedGame');
+const zoneRouter =require('./routes/zone');
 
-const app = express();
-
-app.use(logger('dev'));
+require('dotenv/config'); //todo: delete in prod
+app.use(logger('dev')); //todo: delete in prod
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
+/* Middlewares */
 app.use(authRouter);
 app.use('/user', userRouter);
 app.use('/festival', festivalRouter);
@@ -28,14 +32,16 @@ app.use('/reservation', reservationRouter);
 app.use('/editor', editorRouter);
 app.use('/exhibitor', exhibitorRouter);
 app.use('/contact', contactRouter);
-app.use('/space', spaceFestival);
+app.use('/space', spaceRouter);
+app.use('/reservedGame', reservedGameRouter);
+app.use('/zone', zoneRouter);
 
+/* Default Route */
 app.use((rep, res) => {
     res.status(404).json({error: "route does not exist"})
 });
 
-require('dotenv/config');
-
+/* Connection to DB */
 mongoose.connect(
     process.env.DB_CONNECTION,
     {useNewUrlParser: true, useUnifiedTopology: true},
